a9abacd1773f724579ec3701b9f72394ceb84d4f
大宝哥 <614078730@qq.com>
2021-05-25 10:38:42 +0800

    testDemo.java 第一次提交

---
+++ src/main/java/cn/kgc/test/testDemo.java
@@ -0,0 +1,12 @@
+package cn.kgc.test;
+
+/**
+ * @author 童靖
+ * @date 2021/05/25 10:36
+ * @Created by 云上容景的MACHENIKE
+ */
+public class testDemo {
+    public static void main(String[] args) {
+        System.out.println("第一组提交代码");
+    }
+}
