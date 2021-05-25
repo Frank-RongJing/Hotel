efc3ece84da8a3b6538efc4a8cda05c63a92dede
大宝哥 <614078730@qq.com>
2021-05-25 10:53:04 +0800

    提交本地

--- src/main/java/cn/kgc/test/testDemo.java
+++ src/main/java/cn/kgc/test/testDemo.java
@@ -8,5 +8,8 @@ package cn.kgc.test;
 public class testDemo {
     public static void main(String[] args) {
         System.out.println("第一组提交代码");
+
+
+        System.out.println("B组人员开发代码");
     }
 }
