eae45d0169a61a978df61ab9ac0d01f0b4e44186
大宝哥 <614078730@qq.com>
2021-05-25 10:50:43 +0800

    testDemo.java 第2次提交

--- src/main/java/cn/kgc/test/testDemo.java
+++ src/main/java/cn/kgc/test/testDemo.java
@@ -8,5 +8,6 @@ package cn.kgc.test;
 public class testDemo {
     public static void main(String[] args) {
         System.out.println("第一组提交代码");
+        System.out.println("欸嘿  我又提交了");
     }
 }
